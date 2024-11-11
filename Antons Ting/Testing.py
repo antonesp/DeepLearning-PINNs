e7f1882ca91fc52dde239850e1c305472c85c1fc
antonesp <97282336+antonesp@users.noreply.github.com>
2024-11-11 15:53:29 +0100

    Pulling New Changes

--- Antons Ting/Testing.py
+++ Antons Ting/Testing.py
@@ -1,5 +1,15 @@
 import torch
-import tensorflow as tf
-import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.nn.init as init
+import torch.autograd as autograd
+from torch.nn.parameter import Parameter
 
 
+class PINN(nn.module):
+
+
+    def LossPDE(self,X_train_Nu):
+
+        
